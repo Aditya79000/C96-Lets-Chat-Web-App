462c569d79b6a68ee35f0f32ee056e1b4ce502d9
Aditya79000 <105648681+Aditya79000@users.noreply.github.com>
2022-10-10 17:53:31 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCkTXdklhkYN1S1zWfra899IjQqdbi5RvE",
+    authDomain: "c93-project-8acf0.firebaseapp.com",
+    projectId: "c93-project-8acf0",
+    storageBucket: "c93-project-8acf0.appspot.com",
+    messagingSenderId: "321360568335",
+    appId: "1:321360568335:web:cbf1e845e194b1722b837a"
+  };
+  
